ef9a5775ad539b977538d99635ec4af37359e563
Saivani-S-Naik <150780724+Saivani-S-Naik@users.noreply.github.com>
2023-11-14 13:33:58 +0530

    Add files via upload

---
+++ vani.js
@@ -0,0 +1,26 @@
+
+
+
+function funct()
+{
+   window.location.assign("vanihtml2.html");
+}
+
+function login()
+{
+  var email = document.getElementById("gmail").value;
+  var password = document.getElementById("password").value;
+  if(email == 'abcd@gmail.com'  && password =='12345678')
+  {
+    window.location.assign("vanihtml3.html");
+  }
+  else
+  {
+      alert("You have entered wrong credential ! please enter valid credential");
+  }
+}
+
+function user()
+{
+  var user = document.getElementById("");
+}
